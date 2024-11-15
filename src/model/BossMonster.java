c26001839accf015645dee7e6cc86d0f2450b769
tmdgur23 <dltmdgur08@naver.com>
2024-11-15 22:26:01 +0900

    보스 몬스터 추가

--- src/model/BossMonster.java
+++ src/model/BossMonster.java
@@ -1,6 +1,5 @@
 package model;
 
-<<<<<<< HEAD
 import java.awt.Color;
 import java.awt.FlowLayout;
 
@@ -56,34 +55,4 @@ public class BossMonster extends Opponent {
 		System.out.println(getName() + " 보스 몬스터의 능력치가 상승했습니다! (특수 공격력 증가)");
 	}
 
-=======
-public class BossMonster extends Opponent {
-    private int specialAttackPower;
-
-    // 생성자: 이름, 체력, 특수 공격력 설정
-    public BossMonster(String name, int health, int specialAttackPower) {
-        super(name, health);
-        this.specialAttackPower = specialAttackPower;
-    }
-
-    // 보스 몬스터의 특수 공격 메서드 오버라이드
-    @Override
-    public int specialAttack() {
-        System.out.println(getName() + "이(가) 강력한 특수 공격을 사용합니다!");
-        return specialAttackPower + super.specialAttack(); // 기본 특수 공격력에 보스의 특수 공격력 추가
-    }
-
-    // 보스 몬스터의 피해 감소: 보스가 받는 피해를 20% 줄임
-    @Override
-    public void takeDamage(int damage) {
-        int reducedDamage = (int) (damage * 0.8); // 20% 피해 감소
-        super.takeDamage(reducedDamage);
-        System.out.println(getName() + "이(가) " + reducedDamage + "의 피해를 입었습니다! (보스의 피해 감소 적용)");
-    }
-
-    // 보스 몬스터의 특수 공격력 반환
-    public int getSpecialAttackPower() {
-        return specialAttackPower;
-    }
->>>>>>> 35f4c543b586ee3eacd249c2c8f43679c629fab6
 }
